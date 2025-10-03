abb397c185e1bcfa6ff6f675bfbece22f27ac444
Sergei Egorov <null@egorbos.codes>
2025-10-03 22:58:14 +1000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM mcr.microsoft.com/dotnet/sdk:8.0-alpine AS build-env
+WORKDIR /App
+
+COPY . ./
+RUN dotnet restore && dotnet publish -c Release -o out
+
+FROM mcr.microsoft.com/dotnet/runtime:8.0-alpine
+WORKDIR /App
+COPY --from=build-env /App/out .
+ENTRYPOINT dotnet KafkaTopicsInit.dll
---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System.CommandLine;
+using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+
+var bootstrapServerOption = new Option<string>("--bootstrap-server")
+{
+    Description = "Kafka bootstrap server"
+};
+var topicOption = new Option<string>("--topic")
+{
+    Description = "Topic name"
+};
+var partitionsOption = new Option<int>("--partitions")
+{
+    Description = "Number of partitions",
+    DefaultValueFactory = _ => 1
+};
+var replicationFactorOption = new Option<short>("--replication-factor")
+{
+    Description = "Replication factor",
+    DefaultValueFactory = _ => 1
+};
+
+var rootCommand = new RootCommand("Kafka topic creator");
+rootCommand.Options.Add(bootstrapServerOption);
+rootCommand.Options.Add(topicOption);
+rootCommand.Options.Add(partitionsOption);
+rootCommand.Options.Add(replicationFactorOption);
+
+rootCommand.SetAction(parseResult =>
+{
+    var server = parseResult.GetValue(bootstrapServerOption);
+    var topic = parseResult.GetValue(topicOption);
+    var partitionsCount = parseResult.GetValue(partitionsOption);
+    var replicationFactor = parseResult.GetValue(replicationFactorOption);
+
+    var adminClientConfig = new AdminClientConfig
+    {
+        BootstrapServers = server
+    };
+
+    var adminClient = new AdminClientBuilder(adminClientConfig).Build();
+    var metadata = adminClient.GetMetadata(topic, TimeSpan.FromSeconds(10));
+
+    if (metadata.Topics.Count == 0)
+    {
+        adminClient.CreateTopicsAsync([
+            new TopicSpecification { Name = topic, NumPartitions = partitionsCount, ReplicationFactor = replicationFactor }
+        ]).Wait();
+    }
+});
+
+var result = rootCommand.Parse(args);
+return result.Invoke();
